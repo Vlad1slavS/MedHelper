883a9f903762eff6c358eec6f73d1f4e3a00b8fd
vladstepanov <xstep759@gmail.com>
2025-03-23 13:16:16 +0900

    12121

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Chat from "./components/Chat";
-import ChatInput from "./components/ChatInput";
 import TopBar from "./components/TopBar";
 
 export default function App() {
